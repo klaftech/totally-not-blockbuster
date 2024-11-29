5a3fb05109c2dc89bc1dd6f1c1929e86d9da82ef
Elchonon <ekn400@gmail.com>
2024-11-28 21:06:25 -0500

    added CartContainer placeholder

---
+++ src/components/CartContainer.jsx
@@ -0,0 +1,14 @@
+import Header from './Header'
+
+function CartContainer() {
+    return (
+        <>
+            <Header />
+            <div style={{padding: "7%"}}>
+                Under Construction
+            </div>
+        </>
+    );
+}
+
+export default CartContainer;
