130347f119c03bfaeb9dfef3bdf8c56fbe802e20
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-12-04 02:53:05 -0500

    a lot of styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,18 +2,18 @@ import { NavLink } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import "../assets/css/navbar.css"
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand as={NavLink} to="/">Totally Not Blockbuster</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/" className="navbar-title">Totally Not Blockbuster</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-            <Nav.Link as={NavLink} to="/wishlist">Wishlist</Nav.Link>
-            <Nav.Link as={NavLink} to="/cart">Cart</Nav.Link>
+            <Nav.Link as={NavLink} to="/" className="navbar-item">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/wishlist" className="navbar-item">Wishlist</Nav.Link>
+            <Nav.Link as={NavLink} to="/cart" className="navbar-item">Cart</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
