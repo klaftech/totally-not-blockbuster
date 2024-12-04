80a4c22aaca6988502e094d07529aded34b2345d
Elchonon <ekn400@gmail.com>
2024-12-04 10:14:25 -0500

    github pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import "../assets/css/navbar.css"
+
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
