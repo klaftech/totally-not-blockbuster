189fe2f2b489205224eb27bf7d2d18d62cfdb2e1
Elchonon <ekn400@gmail.com>
2024-12-04 19:02:25 -0500

    abstract out serverBaseUrl & fixed request form header

---
+++ src/assets/serverBaseUrl.js
@@ -0,0 +1,5 @@
+//using local json-server
+//export const serverBaseUrl = "http://localhost:3000"
+
+//using render.com
+export const serverBaseUrl = "https://totally-not-blockbuster-api.onrender.com/api"
